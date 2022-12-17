ece58f078a40655bdd1eff38e36e28bab846a3ec
Faultz <48635770+Faultz@users.noreply.github.com>
2022-12-17 07:53:05 +0000

    weird issue

--- boosie_ui/util/scheduler.h
+++ boosie_ui/util/scheduler.h
@@ -7,8 +7,7 @@ class scheduler
 public:
 	enum thread
 	{
-		render_foreground,
-		render_background
+		render
 	};
 
 
@@ -16,9 +15,9 @@ public:
 	static void start();
 	static void stop();
 
-	static void schedule(menu_callback_t callback, uint64_t arg = 0, uint64_t interval = 0, thread thread = render_foreground);
-	static void once(menu_callback_t callback, thread thread = render_foreground);
-	static void delay(menu_callback_t callback, uint64_t delay, thread thread = render_foreground);
+	static void schedule(menu_callback_t callback, uint64_t arg = 0, uint64_t interval = 0, thread thread = render);
+	static void once(menu_callback_t callback, thread thread = render);
+	static void delay(menu_callback_t callback, uint64_t delay, thread thread = render);
 
 	struct task
 	{
@@ -30,14 +29,11 @@ public:
 		bool is_temp;
 	};
 
-	static std::vector<task> main_tasks;
-	static std::vector<task> render_bkg_tasks;
-	static std::vector<task> render_fore_tasks;
+	static std::vector<task> render_tasks;
 
 	static std::vector<task>* get_tasks(thread thread);
 
 	static void ScrPlace_EndFrame();
-	static void RB_ExecuteRenderCommandsLoop(const void* cmd);
 
 	static void execute(thread thread);
 };
